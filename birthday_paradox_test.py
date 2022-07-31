6ce14e1376dffb9a2a0d98c72f8bfca30f043a57
im <idevm5@ya.ru>
2022-07-31 12:28:10 +0300

    first commit

---
+++ birthday_paradox_test.py
@@ -0,0 +1,9 @@
+import unittest
+import birthday_paradox
+
+class BirthdayParadoxTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.bp = birthday_paradox
+
+    def test(self):
+        pass
